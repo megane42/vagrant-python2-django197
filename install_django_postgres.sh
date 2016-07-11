4ba6469a7017f0faaddd48362833a10cd3dc0d0d
megane42 <h.kzm.gm@gmail.com>
2016-07-12 00:07:48 +0900

    add libpg-dev and python-dev

--- install_django_postgres.sh
+++ install_django_postgres.sh
@@ -1,6 +1,8 @@
 # Install Django and PostgreSQL
 apt-get update
+apt-get install -y python-dev
 apt-get install -y python-pip
 apt-get install -y postgresql-9.4
+apt-get install -y libpq-dev
 pip install --upgrade pip
 pip install django==1.9.7
